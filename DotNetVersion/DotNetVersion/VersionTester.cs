5dbec3f29b91775d0f5f07119a6e678e47ad3686
Scott Stemen <Scott@SkyK9.com>
2019-04-05 14:20:15 -0700

    Saving Updates

---
+++ DotNetVersion/DotNetVersion/VersionTester.cs
@@ -0,0 +1,13 @@
+﻿namespace DotNetVersion
+{
+  using System;
+  public class VersionTester
+  {
+    public static void Main(string[] args)
+    {
+      Console.WriteLine("Hello");
+      Console.WriteLine($"Version: {Environment.Version}");
+      Console.ReadLine();
+    }
+  }
+}
